db9c32fc682d8931610827bd61384a768a612ebe
Frozn11 <karim.elkhawankey@gmail.com>
2024-05-12 18:01:31 +0300

    bottele

--- Lesons/PythonPrograms/Python lvl 3/M2L3/logic.py
+++ Lesons/PythonPrograms/Python lvl 3/M2L3/logic.py
@@ -34,11 +34,48 @@ class Question:
                 markup.add(InlineKeyboardButton(option, callback_data='wrong'))
         return markup
 
+class opsions:
+    def __init__(self, text, answer_id, *options):
+        self.__text = text
+        self.__answer_id = answer_id
+        self.options = options
+
+    @property 
+    def text(self):
+        return self.__text
+
+    def gen_markup(self):
+        # Задание 3 - Создай метод для генерации Inline клавиатуры
+        markup = InlineKeyboardMarkup()
+        markup.row_width = len(self.options)
+
+        for i, option in enumerate(self.options):
+            # Если порядковый номер ответа - номер правильного ответа, то:
+            if i == self.__answer_id:
+                markup.add(InlineKeyboardButton(option, callback_data='Memes'))
+            else:
+                markup.add(InlineKeyboardButton(option, callback_data='Games'))
+        return markup
+
+
+
 # Задание 4 - заполни список своими вопросами
+
+quiz_opsions = [
+    opsions("Memes or Games",0,"Memes", "Games")
+]
+
+quiz_questions_games = [
+    Question("imagesGames/image1.png","откуда этот персонаж",0,"Minecraft","Dark Souls III","Counter-Strike 2"),
+    Question("imagesGames/image2.jpg","что за игра",2,"7 Days to Die","Valheim","Garry's Mod"),
+    Question("images","В какой игре вы выживаете на плоту",1,"Postal 2","Raft","Soundpad","Minecraft"),
+    Question("images","В какой игре вы играйте за гнома и копаете ресурсы на разных астероидах и убиваете Жуков",0,"Deep rock galactic","Don't starve together","Payday 2","Forza Horizon 4"),
+    Question("images","for Rock and Stone",0,"ROCK AND SOTNE","что?")
+]
 
 quiz_questions = [
     Question("None","Что котики делают, когда никто их не видит?", 1, "Спят", "Пишут мемы"),
     Question("None","Как котики выражают свою любовь?", 0, "Громким мурлыканием", "Отправляют фото на Instagram", "Гавкают"),
-    Question("images/image1.png","Это кошка?", 0, "да", "нет"),
+    Question("imagesMemes/image1.png","Это кошка?", 0, "да", "нет"),
     Question("None","Какие книги котики любят читать?", 3, "Обретение вашего внутреннего урр-мирения", "Тайм-менеджмент или как выделить 18 часов в день для сна", "101 способ уснуть на 5 минут раньше, чем хозяин", "Пособие по управлению людьми")
 ]
--- Lesons/PythonPrograms/Python lvl 3/M2L3/main.py
+++ Lesons/PythonPrograms/Python lvl 3/M2L3/main.py
@@ -1,7 +1,7 @@
 import telebot
 from config import token
 # Задание 7 - испортируй команду defaultdict
-from logic import quiz_questions
+from logic import quiz_questions, quiz_opsions, quiz_questions_games
 from collections import defaultdict
 
 user_responses = {} 
@@ -9,7 +9,7 @@ user_responses = {}
 
 bot = telebot.TeleBot(token)
 points = defaultdict(int)
-
+gamemode = defaultdict(int)
 
 def send_question(chat_id):
     try:
@@ -18,12 +18,35 @@ def send_question(chat_id):
         print(f"no photo, Error message: {e}")
     bot.send_message(chat_id, quiz_questions[user_responses[chat_id]].text, reply_markup=quiz_questions[user_responses[chat_id]].gen_markup())
 
+def send_opsions(chat_id):
+    bot.send_message(chat_id, quiz_opsions[user_responses[chat_id]].text, reply_markup=quiz_opsions[user_responses[chat_id]].gen_markup())
 
-
+def send_question_games(chat_id):
+    try:   
+        bot.send_chat_action(chat_id, 'typing')
+        bot.send_photo(chat_id, photo=open(quiz_questions_games[user_responses[chat_id]].photos, 'rb'))
+    except Exception as e:
+        print(f"no photo, Error message: {e}")
+    bot.send_message(chat_id, quiz_questions_games[user_responses[chat_id]].text, reply_markup=quiz_questions_games[user_responses[chat_id]].gen_markup())
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    if call.data == "correct":
+
+    if call.data == 'Games':
+        bot.answer_callback_query(call.id, "Games")
+        gamemode[call.message.chat.id] += 1
+        send_question_games(call.message.chat.id)
+        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
+        return
+        
+    elif call.data == "Memes":
+        bot.answer_callback_query(call.id, "Memes")
+        send_question(call.message.chat.id)
+        gamemode[call.message.chat.id] += 0
+        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
+        return    
+    
+    elif call.data == "correct":
         bot.answer_callback_query(call.id, "Answer is correct")
         bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
         points[call.message.chat.id] += 1
@@ -33,12 +56,23 @@ def callback_query(call):
         bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id, reply_markup=None)
         ## msg = bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f'{quiz_questions[user_responses[chat_id]].text}', reply_markup=None)
     # Задание 5 - реализуй счетчик вопросов
+
     user_responses[call.message.chat.id] += 1
-    if user_responses[call.message.chat.id] >= len(quiz_questions):
-        bot.send_message(call.message.chat.id, f'The End, you got {points[call.message.chat.id]} points')
-        points[call.message.chat.id] = 0   
+    if gamemode[call.message.chat.id] == 1:
+        if user_responses[call.message.chat.id] >= len(quiz_questions_games):
+            bot.send_message(call.message.chat.id, f'The End, you got {points[call.message.chat.id]} points')
+            gamemode[call.message.chat.id] = 0
+            points[call.message.chat.id] = 0   
+        else:
+            bot.send_chat_action(call.message.chat.id, 'typing')
+            send_question_games(call.message.chat.id)
     else:
-        send_question(call.message.chat.id)
+        if user_responses[call.message.chat.id] >= len(quiz_questions):
+            bot.send_message(call.message.chat.id, f'The End, you got {points[call.message.chat.id]} points')
+            points[call.message.chat.id] = 0   
+        else:
+            bot.send_chat_action(call.message.chat.id, 'typing')
+            send_question(call.message.chat.id)
 
 
 
@@ -53,7 +87,8 @@ def callback_query(call):
 def start(message):
     if message.chat.id:
         user_responses[message.chat.id] = 0
-        send_question(message.chat.id)
+        gamemode[message.chat.id] = 0
+        send_opsions(message.chat.id)
 
 
 bot.infinity_polling()
